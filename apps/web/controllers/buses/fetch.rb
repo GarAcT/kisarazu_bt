b9f03f63d21cae7f2de95602c69ff0518b6fc046
semi11 <stand.say.me@gmai.com>
2018-12-06 19:35:53 +0900

    update create only valid bus info (#1)

--- apps/web/controllers/buses/fetch.rb
+++ apps/web/controllers/buses/fetch.rb
@@ -8,10 +8,12 @@ module Web
       class Fetch
         include Web::Action
 
+        expose :message
+
         def call(params)
           BusRepository.new.clear
           rosenNum = 11
-          for num in 1..rosenNum do
+          for num in 1..rosenNum
             params = URI.encode_www_form({busid: num})
             uri = URI.parse("http://tutujibus.com/busLookup.php?#{params}")
 
@@ -22,15 +24,20 @@ module Web
             end
             begin
               case response
-              when Net::HTTPSuccess
+              when Net::HTTPSuccess #2xx
                 @result = JSON.parse(response.body.split('(')[1].sub(')',''))#.gsub(/(\w+):/, '"\1":'))
                 #BusRepository.new.create({ isRunning: true, datetime: '123', busid: 1, rosenid: 2, binid: 3, latitude: 1.1, longitude: 2.3, speed: 3, direction:3, destination: 'aaa', isdelay: false })
-                BusRepository.new.create({ isRunning: @result["isRunning"], datetime: @result["datetime"], busid: @result["busid"], rosenid: @result["rosenid"], binid: @result["binid"], latitude: @result["latitude"], longitude: @result["longitude"], speed: @result["speed"], direction: @result["direction"], destination: @result["destination"], isdelay: @result["isdelay"]})
-              when Net::HTTPRedirection
+                if !@result.empty? and @result["isRunning"]
+                  BusRepository.new.create({ isRunning: @result["isRunning"], datetime: @result["datetime"], busid: @result["busid"], rosenid: @result["rosenid"], binid: @result["binid"], latitude: @result["latitude"], longitude: @result["longitude"], speed: @result["speed"], direction: @result["direction"], destination: @result["destination"], isdelay: @result["isdelay"]})
+                  @message = "fetched"
+                end
+              when Net::HTTPRedirection #3xx
                 @message = "Redirection: code=#{response.code} message=#{response.message}"
               else
                 @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
               end
+
+            #error handling
             rescue IOError => e
               @message = e.message
             rescue TimeoutError => e
