f4c3fb25424409aa265fe67b6b907b760ee329e8
semi11 <stand.say.me@gmai.com>
2018-12-06 13:19:14 +0900

    update diplay all bus info (#1)

--- apps/web/controllers/buses/fetch.rb
+++ apps/web/controllers/buses/fetch.rb
@@ -10,36 +10,39 @@ module Web
 
         def call(params)
           BusRepository.new.clear
-          #For num in 1..11
-          params = URI.encode_www_form({busid: '3'})
-          uri = URI.parse("http://tutujibus.com/busLookup.php?#{params}")
+          rosenNum = 11
+          for num in 1..rosenNum do
+            params = URI.encode_www_form({busid: num})
+            uri = URI.parse("http://tutujibus.com/busLookup.php?#{params}")
 
-          response = Net::HTTP.start(uri.host, uri.port) do |http|
-            http.open_timeout = 5
-            http.read_timeout = 10
-            http.get(uri.request_uri)
-          end
-          begin
-            case response
-            when Net::HTTPSuccess
-              @result = JSON.parse(response.body.split('(')[1].sub(')',''))#.gsub(/(\w+):/, '"\1":'))
-              #BusRepository.new.create({ isRunning: true, datetime: '123', busid: 1, rosenid: 2, binid: 3, latitude: 1.1, longitude: 2.3, speed: 3, direction:3, destination: 'aaa', isdelay: false })
-              BusRepository.new.create({ isRunning: @result["isRunning"], datetime: @result["datetime"], busid: @result["busid"], rosenid: @result["rosenid"], binid: @result["binid"], latitude: @result["latitude"], longitude: @result["longitude"], speed: @result["speed"], direction: @result["direction"], destination: @result["destination"], isdelay: @result["isdelay"]})
-              redirect_to '/buses'
-            when Net::HTTPRedirection
-              @message = "Redirection: code=#{response.code} message=#{response.message}"
-            else
-              @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+            response = Net::HTTP.start(uri.host, uri.port) do |http|
+              http.open_timeout = 5
+              http.read_timeout = 10
+              http.get(uri.request_uri)
             end
-          rescue IOError => e
-            @message = e.message
-          rescue TimeoutError => e
-            @message = e.message
-          rescue JSON::ParserError => e
-            @message = e.message
-          rescue => e
-            @message = e.message
+            begin
+              case response
+              when Net::HTTPSuccess
+                @result = JSON.parse(response.body.split('(')[1].sub(')',''))#.gsub(/(\w+):/, '"\1":'))
+                #BusRepository.new.create({ isRunning: true, datetime: '123', busid: 1, rosenid: 2, binid: 3, latitude: 1.1, longitude: 2.3, speed: 3, direction:3, destination: 'aaa', isdelay: false })
+                BusRepository.new.create({ isRunning: @result["isRunning"], datetime: @result["datetime"], busid: @result["busid"], rosenid: @result["rosenid"], binid: @result["binid"], latitude: @result["latitude"], longitude: @result["longitude"], speed: @result["speed"], direction: @result["direction"], destination: @result["destination"], isdelay: @result["isdelay"]})
+              when Net::HTTPRedirection
+                @message = "Redirection: code=#{response.code} message=#{response.message}"
+              else
+                @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+              end
+            rescue IOError => e
+              @message = e.message
+            rescue TimeoutError => e
+              @message = e.message
+            rescue JSON::ParserError => e
+              @message = e.message
+            rescue => e
+              @message = e.message
+            end
           end
+
+          redirect_to '/buses'
         end
       end
     end
